961073655870085c8cd2eb5678463e102a7c657d
IrwinBeyond <irwinahmad123@gmail.com>
2025-11-23 14:04:57 +0700

    Fixing attendance update feature

--- app/Http/Controllers/Admin/AttendanceController.php
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Attendance;
 use App\Models\Employee;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class AttendanceController extends Controller
 {
